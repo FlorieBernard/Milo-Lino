ad2eec41932396a5ad61e8ba1d68ae6df538eb26
Florie Bernard <>
2026-03-16 17:26:57 +0100

    CODE VFX RECEPTION CORRIGE

--- Assets/SCRIPT/PlayerMovementMilo.cs
+++ Assets/SCRIPT/PlayerMovementMilo.cs
@@ -1,15 +1,19 @@
-using UnityEngine;
+ï»¿using UnityEngine;
 
 public class PlayerMovementMilo : MonoBehaviour
+
+
 {
+    public ParticleSystem smokePrefab;
     public float horizontal;
     public float speed = 8f;
     public float jumpingPower = 16f;
-    // public bool isFacingRight = true; <-- n'a plus aucun intérêt
+    // public bool isFacingRight = true; <-- n'a plus aucun intÃ©rÃªt
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private bool inGround = true;
 
     // Update is called once per frame
     void Update()
@@ -19,16 +23,34 @@ public class PlayerMovementMilo : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+
         }
         if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocityX, rb.linearVelocity.y * 0.5f);
+            
         }
 
-        if (horizontal != 0f)// <-- j'ai ajouté une vérification pour que le flip soit actif uniquement durant le déplacement
+        if (horizontal != 0f)// <-- j'ai ajoutÃ© une vÃ©rification pour que le flip soit actif uniquement durant le dÃ©placement
         {
-            Flip(horizontal); // <-- Flip() + la direction en paramètre
+            Flip(horizontal); // <-- Flip() + la direction en paramÃ¨tre
         }
+
+
+        if (!isGrounded())
+        {
+            inGround = false;
+        }
+        else
+        {
+            if (!inGround)
+            {
+                SpawnSmoke();
+                Debug.Log("Is landing");
+                inGround = true;
+            }
+        }
+        
     }
 
     private void FixedUpdate()
@@ -43,11 +65,24 @@ public class PlayerMovementMilo : MonoBehaviour
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
-    private void Flip(float localScaleDirection) // <-- j'ai ajouté un paramètre afin d'y apliquer la direction de Milo
+    private void Flip(float localScaleDirection) // <-- j'ai ajoutÃ© un paramÃ¨tre afin d'y apliquer la direction de Milo
     {
-        // isFacingRight = !isFacingRight;  <-- n'a plus aucun intérêt
+        // isFacingRight = !isFacingRight;  <-- n'a plus aucun intÃ©rÃªt
         Vector3 localScale = transform.localScale;
-        localScale.x = localScaleDirection; // <-- j'ai remplacé *=-1 par le paramètre çi-dessus
+        localScale.x = localScaleDirection; // <-- j'ai remplacÃ© *=-1 par le paramÃ¨tre Ã§i-dessus
         transform.localScale = localScale;
+
+    }
+
+
+    void SpawnSmoke()
+    {
+        if (smokePrefab == null) return;
+
+        Vector3 spawnPos = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+        ParticleSystem smoke = Instantiate(smokePrefab, spawnPos, Quaternion.identity);
+        smoke.Play();
+
+        Destroy(smoke.gameObject, smoke.main.duration + smoke.main.startLifetime.constantMax);
     }
 }
